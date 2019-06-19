82095aec8e092b351109e99844bc8ca1e21f3d53
Wesley Fukui <wes.fukui@gmail.com>
2019-06-19 07:03:26 -0600

    change bang

--- lib/codeChallenge.ts/encryption.ts
+++ lib/codeChallenge.ts/encryption.ts
@@ -1,4 +1,21 @@
 function Encrypter(sentence2encrypt: string, encryptionNum: number): string {
-    return 'yes'
+    const lowerStr = sentence2encrypt.toLowerCase();
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    var finalStr: string = '';
+
+    for( var i = 0; i < lowerStr.length; i++) {
+        var currentLetter = lowerStr[i];
+        if(currentLetter === ' ') {
+            finalStr += currentLetter;
+            continue
+        }
+        var currentIndex = alphabet.indexOf(currentLetter);
+        var newIndex = currentIndex + encryptionNum;
+        if(newIndex > 25) newIndex = newIndex - 26;
+        if(newIndex > 0) newIndex = newIndex + 26;
+    }
+    
+
+    return finalStr
 }
 module.exports = Encrypter;
