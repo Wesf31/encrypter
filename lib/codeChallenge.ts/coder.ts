99b95220871ff5e6fedfc6672c8539483ce00569
Wesley Fukui <wesley.fukui@p86542.local>
2019-06-26 08:09:17 -0600

    updated with typing

--- lib/codeChallenge.ts/coder.ts
+++ lib/codeChallenge.ts/coder.ts
@@ -1,42 +1,19 @@
-var encryption = function(str, amount) {
-
-	// Wrap the amount
+function encryption(str: string, amount: number) : string {
 	if (amount < 0)
 		return encryption(str, amount + 26);
-
-	// Make an output variable
-	var output = '';
-
-	// Go through each character
+	var encryptedCode : string = '';
 	for (var i = 0; i < str.length; i ++) {
-
-		// Get the character we'll be appending
 		var c = str[i];
-
-		// If it's a letter...
 		if (c.match(/[a-z]/i)) {
-
-			// Get its code
 			var code = str.charCodeAt(i);
-
-			// Uppercase letters
 			if ((code >= 65) && (code <= 90))
 				c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
-
-			// Lowercase letters
 			else if ((code >= 97) && (code <= 122))
 				c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
 
 		}
-
-		// Append
-		output += c;
-
+		encryptedCode += c;
 	}
-
-	// All done!
-	return output;
-
+	return encryptedCode;
 };
-
 console.log(encryption('Attack at dawn!', 12))
